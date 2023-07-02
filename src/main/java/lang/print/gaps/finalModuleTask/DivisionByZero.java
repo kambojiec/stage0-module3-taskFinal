1d4b8b146aece5ddb161741c55db071405554c6f
Vlad Kupchik <vladkupchik15@gmail.com>
2023-07-03 02:50:52 +0300

    123456

--- src/main/java/lang/print/gaps/finalModuleTask/DivisionByZero.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DivisionByZero.java
@@ -3,6 +3,6 @@ package lang.print.gaps.finalModuleTask;
 public class DivisionByZero {
     public static void main(String[] args) {
         var number = 10;
-        System.out.println(number/0);
+        System.out.println(number/0.0);
     }
 }
